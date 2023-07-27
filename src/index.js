ddb8e334fe35580584979942fe6df069b70f78b3
Deepak584953 <dk059511@gmail.com>
2023-07-27 23:41:37 +0530

    scond commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ const  express  = require("express");
 const {PORT} = require('./config/config')
 
 const app = express()
-
+const runServer = ()=>{
 app.listen(PORT,()=>{
-    
+
     console.log(`server start on port no. ${PORT}`)
 })
+}
+
+runServer()
